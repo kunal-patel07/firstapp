7f48098f4e6e8bae8f8ec414c15c9ea777dec0a0
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-05-14 17:36:23 +0530

    used parallel api calls for all the apis using promis.All

--- app/(marketing)/services/webDev/page.js
+++ app/(marketing)/services/webDev/page.js
@@ -1,17 +1,53 @@
+ //  export let dynamic = "force-dynamic"
+// 'use client';
+// import { useState } from 'react';
+ 
+async function fetchData(url) {
+    let data = await fetch(url)
+    return await data.json()   
+}
+//parallel call for all apis using promise.All
+let urls = [
+  "https://jsonplaceholder.typicode.com/todos?_limit=5",
+  "https://procodrr.vercel.app/?sleep=1000",
+  "https://procodrr.vercel.app/?sleep=5000",
+];
 
- export let dynamic = "force-dynamic"
-let webDev= ()=>{
+let webDev = async() => {
+    let [todos, data2s, data3s] = await Promise.all(
+        urls.map(url => fetchData(url))
+    )
     
-    console.log("render as you want because it static")
+    // const [completedTodos, setCompletedTodos] = useState({});
+
+    // const handleCheckboxChange = (title) => {
+    //     setCompletedTodos(prev => ({
+    //         ...prev,
+    //         [title]: !prev[title]
+    //     }));
+    // };
 
     return (
         <div>
-            <p>Front end dev  </p>
+            {todos.map(({title, completed}) => (
+                <div key={title}>
+                    <p>{title}</p>
+                    {/* <input 
+                        type='checkbox' 
+                        checked={completedTodos[title] ?? completed} 
+                        onChange={() => handleCheckboxChange(title)}
+                    /> */}
+                </div>
+            ))}
+            <h3>{JSON.stringify(data2s)}</h3>
+            <h3>{JSON.stringify(data3s)}</h3>
+            
+            <p>Front end dev </p>
             <p>Back end dev</p>
             <p>Full stack Dev </p>
             <p>MERN stack dev </p>
         </div>
-    )
-}
+    );
+};
 
-export default webDev;  
+export default webDev;
